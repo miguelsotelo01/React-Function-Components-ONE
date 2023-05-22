ddbd1ffd9c200b550d34c8d0ac4cdc9f41cbe771
miguelsotelo01 <miky.a.sotelo.97@gmail.com>
2023-05-21 23:43:58 -0300

    module 2 finish

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
+import Container from '@mui/material/Container';
 import FormSingUp from './components/FormSingUp';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
-    <>
-    <h1>formulario registro</h1>
+    <Container component="section" maxWidth="sm">
+    <Typography variant='h3' align='center' component="h1">Formulario Registro</Typography>
    <FormSingUp/>
-    </>
+    </Container>
   );
 }
 
--- src/components/FormSingUp.jsx
+++ src/components/FormSingUp.jsx
@@ -1,16 +1,47 @@
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Switch from '@mui/material/Switch';
+import FormGroup  from '@mui/material/FormGroup';
+import  FormControlLabel from '@mui/material/FormControlLabel';
 function FormSingUp (){
     return <form>
-        <label>nombre</label>
-        <input type="text" />
-        <label>apellidos</label>
-        <input type="text" />
-        <label>correo electronico</label>
-        <input type="email" />
-        <label>promociones</label>
-        <input type="checkbox" />
-        <label>novedades</label>
-        <input type="checkbox" />
+       
+        <TextField 
+        id="name" 
+        label="Nombre" 
+        variant="outlined" 
+        fullWidth 
+        margin='normal'
+        />
+        <TextField 
+        id="lastName" 
+        label="Apellidos" 
+        variant="outlined" 
+        color='error' 
+        fullWidth 
+        margin='normal'
+        />
+        <TextField 
+        id="email" 
+        label="email" 
+        variant="outlined"  
+        fullWidth 
+        margin='normal'
+        />
+    <FormGroup>
+        <FormControlLabel 
+        control={
+        <Switch defaultChecked/>
+        } 
+        label="promociones"
+        />
+        <FormControlLabel 
+        control={
+        <Switch defaultChecked/>
+        } 
+        label="novedades"
+        />
+    </FormGroup>
         <Button variant="contained" color="error">registrarse</Button>
 
     </form>
