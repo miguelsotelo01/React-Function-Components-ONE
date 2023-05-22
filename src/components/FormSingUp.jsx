5848f147a81e79f75a5f88f52f09496afcd8f6d1
miguelsotelo01 <miky.a.sotelo.97@gmail.com>
2023-05-22 16:02:31 -0300

    module 3 finish

--- src/components/FormSingUp.jsx
+++ src/components/FormSingUp.jsx
@@ -1,9 +1,14 @@
+import {useState, useEffect} from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Switch from '@mui/material/Switch';
 import FormGroup  from '@mui/material/FormGroup';
 import  FormControlLabel from '@mui/material/FormControlLabel';
 function FormSingUp (){
+    const [name,setName]=useState("")
+    useEffect(() =>{
+        console.log("name cambio:", name);
+    },[name])
     return <form>
        
         <TextField 
@@ -12,6 +17,11 @@ function FormSingUp (){
         variant="outlined" 
         fullWidth 
         margin='normal'
+        onChange={(e)=>{
+            console.log(e.target.value);
+            setName(e.target.value);
+        }}
+        value={name}
         />
         <TextField 
         id="lastName" 
